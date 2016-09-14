1fe829083c067a1844d9c4fd5892dd58dcbd3d00
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-09-14 20:37:12 +0200

    add patch to empty stale dns leases on boot

--- mf910-reboot/install.sh
+++ mf910-reboot/install.sh
@@ -5,6 +5,8 @@ set -e
 echo "Installing reboot script."
 
 DOWNLOAD_LOC="" # HTTP ONLY
+wget $DOWNLOAD_LOC/dnsmasq.patch -O /tmp/dnsmasq.patch
+patch /etc/init.d/dnsmasq /tmp/dnsmasq.patch
 wget $DOWNLOAD_LOC/mf910-reboot.sh -O /usr/bin/mf910-reboot.sh
 chmod +x /usr/bin/mf910-reboot.sh
 wget $DOWNLOAD_LOC/mf910-reboot.init -O /etc/init.d/mf910-reboot
