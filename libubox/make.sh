88148763ea14f00b2efaee6d99890a817b3a02c3
Thomas Hirsch <thomas.hirsch@celerway.com>
2019-07-08 07:58:45 +0000

    fix library location for building libuci

--- libubox/make.sh
+++ libubox/make.sh
@@ -6,8 +6,9 @@ cmake ..
 make ubox
 
 cd ..
-mkdir -p deb_dist
-cp *.h deb_dist/usr/local/include/
+mkdir -p deb_dist/usr/local/include/libubox
+mkdir -p deb_dist/usr/local/lib
+cp *.h deb_dist/usr/local/include/libubox/
 cp build/libubox.so deb_dist/usr/local/lib/
 
 rm -r build
