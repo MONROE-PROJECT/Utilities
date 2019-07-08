72f3421b5b81cef79e5baf7e4db102f5f351ec28
Thomas Hirsch <thomas.hirsch@celerway.com>
2019-07-08 08:19:17 +0000

    revert fix for libuci library location

--- libuci/make.sh
+++ libuci/make.sh
@@ -6,10 +6,10 @@ cmake ..
 make uci cli
 
 cd ..
-mkdir -p deb_dist/usr/local/include/uci
+mkdir -p deb_dist/usr/local/include
 mkdir -p deb_dist/usr/local/lib
 mkdir -p deb_dist/usr/local/bin
-cp uci.h uci_config.h uci_blob.h ucimap.h deb_dist/usr/local/include/uci/
+cp uci.h uci_config.h uci_blob.h ucimap.h deb_dist/usr/local/include/
 cp build/libuci.so deb_dist/usr/local/lib/
 cp build/uci deb_dist/usr/local/bin/
 
